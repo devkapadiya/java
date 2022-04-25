b249ddcad8bc1a1e298ef802f4ed9e7e058ebd7b
devkapadiya <kapadiyadev4@gmail.com>
2022-04-25 14:22:55 +0530

    add file

---
+++ dollartorupees.java
@@ -0,0 +1,16 @@
+package java_sem_4;
+
+import java.util.Scanner;
+
+public class dollartorupees {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the amount in dollar=>");
+		int d=sc.nextInt();
+		System.out.println("dollar into ruppes=>"+(d*72));
+
+	}
+
+}
